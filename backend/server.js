7252d30e88223d07cfaca652291e5da48aeac3ec
RuisKevin <jhosonrobert609@gmail.com>
2023-09-15 04:42:04 -0300

    react

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import data from './data.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URL);
+const app = express();
+
+app.get('/api/products', (req, res) => {
+  res.send(data.products);
+});
+app.get('/api/products/slug/:slug', (req, res) => {
+  const product = data.products.find((x) => x.slug === req.params.slug);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product Not Found' });
+  }
+});
+app.get('/api/products/:_id', (req, res) => {
+  const product = data.products.find((x) => x._id === req.params._id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product Not Found' });
+  }
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`serve at http://localhost:${port}`);
+});
